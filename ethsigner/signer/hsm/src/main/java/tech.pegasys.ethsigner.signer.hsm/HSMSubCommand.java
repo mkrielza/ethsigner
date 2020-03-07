1eca7d24802ebb516650f7ff10a0a85cb0ae2a53
mkrielza <marelize.kriel@adhara.io>
2020-03-07 06:40:20 +0200

    Some testing

---
+++ ethsigner/signer/hsm/src/main/java/tech.pegasys.ethsigner.signer.hsm/HSMSubCommand.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.ethsigner.signer.hsm;
+
+import static tech.pegasys.ethsigner.DefaultCommandValues.MANDATORY_FILE_FORMAT_HELP;
+
+import tech.pegasys.ethsigner.SignerSubCommand;
+import tech.pegasys.ethsigner.TransactionSignerInitializationException;
+import tech.pegasys.ethsigner.core.signing.SingleTransactionSignerProvider;
+import tech.pegasys.ethsigner.core.signing.TransactionSigner;
+import tech.pegasys.ethsigner.core.signing.TransactionSignerProvider;
+
+import java.nio.file.Path;
+
+import com.google.common.base.MoreObjects;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Spec;
+
+/** File-based authentication related sub-command */
+@Command(
+    name = HSMSubCommand.COMMAND_NAME,
+    description = "Sign transactions with a key stored in an HSM.",
+    mixinStandardHelpOptions = true)
+public class HSMSubCommand extends SignerSubCommand {
+
+  public static final String COMMAND_NAME = "hsm-signer";
+
+  public HSMSubCommand() {}
+
+  @SuppressWarnings("unused") // Picocli injects reference to command spec
+  @Spec
+  private CommandLine.Model.CommandSpec spec;
+
+  @Option(
+      names = {"-p", "--password-file"},
+      description = "The path to a file containing the password used to decrypt the keyfile.",
+      required = true,
+      paramLabel = MANDATORY_FILE_FORMAT_HELP,
+      arity = "1")
+  private Path passwordFilePath;
+
+  @SuppressWarnings("FieldMayBeFinal") // Because PicoCLI requires Strings to not be final.
+  @Option(
+      names = {"-k", "--key-file"},
+      description = "The path to a file containing the key used to sign transactions.",
+      required = true,
+      paramLabel = MANDATORY_FILE_FORMAT_HELP,
+      arity = "1")
+  private Path keyFilePath;
+
+  private TransactionSigner createSigner() throws TransactionSignerInitializationException {
+    return HSMSignerFactory.createSigner(keyFilePath, passwordFilePath);
+  }
+
+  @Override
+  public TransactionSignerProvider createSignerFactory()
+      throws TransactionSignerInitializationException {
+    return new SingleTransactionSignerProvider(createSigner());
+  }
+
+  @Override
+  public String getCommandName() {
+    return COMMAND_NAME;
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("passwordFilePath", passwordFilePath)
+        .add("keyFilePath", keyFilePath)
+        .toString();
+  }
+}
