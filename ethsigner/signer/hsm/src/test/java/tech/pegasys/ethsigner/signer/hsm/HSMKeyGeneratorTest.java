d8fef564cdcdef6848163860536cecb4df49a0e8
mkrielza <marelize.kriel@adhara.io>
2020-03-23 09:48:34 +0200

    Test problems

---
+++ ethsigner/signer/hsm/src/test/java/tech/pegasys/ethsigner/signer/hsm/HSMKeyGeneratorTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.ethsigner.signer.hsm;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import tech.pegasys.ethsigner.core.generation.KeyGenerator;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class HSMKeyGeneratorTest {
+
+  private static HSMKeyStoreProvider ksp;
+
+  @BeforeAll
+  public static void createProvider() {
+    ksp = mock(HSMKeyStoreProvider.class);
+  }
+
+  @Test
+  public void success() {
+    final KeyGenerator generator =
+        (new HSMKeyGeneratorFactory(ksp, Path.of("/tmp")).getGenerator());
+    assertThat(generator).isNotNull();
+  }
+}
